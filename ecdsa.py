4dc52333bd153b9c4a002c3e13c7ce7bfa9701f0
Jules LEROQUAIS <leroquais.jules@gmail.com>
2024-04-26 17:32:41 +0200

    ecdsa

--- ecdsa.py
+++ ecdsa.py
@@ -1,4 +1,4 @@
-from rfc7448 import x25519, add, computeVcoordinate, mult
+from rfc7748 import x25519, add, computeVcoordinate, mult
 from Crypto.Hash import SHA256
 from random import randint
 from algebra import mod_inv
@@ -9,22 +9,39 @@ ORDER = (2**252 + 27742317777372353535851937790883648493)
 BaseU = 9
 BaseV = computeVcoordinate(BaseU)
 
-
 def H(message):
     h = SHA256.new(message)
     return (int(h.hexdigest(), 16))
 
-def ECDSA_generate_nonce("""TBC"""):
-    return """TBC"""
+def ECDSA_generate_nonce():
+    return randint(1, ORDER - 1)
 
 
-def ECDSA_generate_keys("""TBC"""):
-    return ("""TBC""")
+def ECDSA_generate_keys():
+    x = randint(1, ORDER - 2)
+    Xu, Xv = mult(x, BaseU, BaseV, p)
+    return Xu, Xv, x
+
+def ECDSA_sign(m, x):
+    s = 0
+    r = 0
+    while s == 0:
+        while r == 0:
+            k = ECDSA_generate_nonce()
+            r, _ = mult(k, BaseU, BaseV, p)
+            r = r % ORDER
+        s = ((H(m) + x * r) * pow(k, -1, ORDER)) % ORDER
+    return r, s
 
 
-def ECDSA_sign("""TBC"""):
-    return ("""TBC""")    
-
-
-def ECDSA_verify("""TBC"""):
-    return """TBC"""
+def ECDSA_verify(Xu, Xv, r, s, m):
+    if 0 >= r or r >= ORDER or 0 >= s or s >= ORDER:
+        return False
+    print("first")
+    u1 = (H(m) * pow(s, -1, ORDER)) % ORDER
+    u2 = (r * pow(s, -1, ORDER)) % ORDER
+    w1u, w1v = mult(u1, BaseU, BaseV, p)
+    w2u, w2v = mult(u2, Xu, Xv, p)
+    v, _ = add(w1u, w1v, w2u, w2v, p)
+    v = v % ORDER
+    return v == r
